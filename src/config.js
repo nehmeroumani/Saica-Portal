c3645b312c25ed7eb1f4804b51629d7140258004
kassemshehady@gmail.com <kassemshehady@gmail.com>
2020-07-25 15:52:34 +0300

    icon

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-//export const Config = { apiUrl: 'https://localhost:44327' };
+export const Config = { apiUrl: 'https://localhost:44327' };
 
-export const Config = { apiUrl: 'http://163.172.70.36:91' };
+//export const Config = { apiUrl: 'http://172.105.92.8:81' };
