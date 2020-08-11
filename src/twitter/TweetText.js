379b59feab3c3d6a81de568a7c0be70a40f74556
Nehme Roumani <nehmeroumani@gmail.com>
2020-08-11 22:27:16 +0300

    update

--- src/twitter/TweetText.js
+++ src/twitter/TweetText.js
@@ -4,7 +4,7 @@ import Twemoji from 'react-twemoji';
 
 
 const TweetText = ({record, source }) => (
-    <Typography className="arabic-tweet"> <Twemoji options={{ className: 'twemoji' }}>{record[source].replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')}</Twemoji> </Typography>
+    <Typography className="arabic-tweet"> <Twemoji options={{ className: 'twemoji' }}>{record[source] && record[source].replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')}</Twemoji> </Typography>
 );
 
 export default TweetText;
