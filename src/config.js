b5338450fa893c000cf2182ed0b51fd898abc1e7
Nehme Roumani <nehmeroumani@gmail.com>
2020-08-12 20:17:14 +0300

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const Config = { apiUrl: 'https://localhost:5001' };
+export const Config = { apiUrl: 'https://localhost:44327' };
 
 //export const Config = { apiUrl: 'http://172.105.92.8:81' };
